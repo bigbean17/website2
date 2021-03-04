f4e49d9e96c93603c0a2dac2158a5c3406d4af6d
yutong <ywan6382@uni.sydney.edu.au>
2021-03-04 20:58:47 +1100

    login/register done

--- models.py
+++ models.py
@@ -1,11 +1,32 @@
-from server import app
-from flask_sqlalchemy import SQLAlchemy
+from server import app, db
+count_user = 0
 
-db = SQLAlchemy(app)
 class User(db.Model):
 
     id=db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), nullable = False, unique=True)
     password = db.Column(db.String(50), nullable = False)
-    email = db.Column(db.String(50), nullable = False)
 
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = True, unique = False)
+    uid = db.Column(db.Integer, db.ForeignKey("user.id"),nullable=False)
+
+
+
+def register(username, password):
+
+    user = User(username=username,password=password)
+    db.session.add(user)
+    db.session.commit()
+
+
+def login(username,password):
+
+    user = db.session.query(User).filter_by(username=username,password=password).all()
+    if(user):
+        return True
+    return False
--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from server import app
 from flask import render_template, redirect, flash, url_for, abort, request
-from models import User
+import models as md
 from form import RegistFrom, LoginForm
+from sqlalchemy  import exc
 
 @app.route("/")
 def index():
@@ -12,14 +13,26 @@ def index():
 def login():
     form_ = LoginForm()
     if form_.validate_on_submit():
-        print(form_.username.data, form_.password.data)
-        return redirect(url_for("index"))
-    
+        if(md.login(form_.username.data,form_.password.data)):
+
+            return redirect(url_for("index"))
+        else:
+            flash("Warning: Wrong username or password")
+            return render_template("login.html",form=form_)
+
     return render_template("login.html",form=form_)
 
 @app.route("/register", methods = ["POST","GET"])
 def register():
     form_ = RegistFrom()
     if form_.validate_on_submit():
-        return redirect(url_for('login'))
+        try:
+            md.register(form_.username.data,form_.password.data)
+            flash("Register success! Please login")
+            return redirect(url_for('login'))
+
+        except exc.IntegrityError as e:
+            flash("Warning: This name has been taken!")
+            return render_template("register.html",form=form_)
+
     return render_template("register.html",form=form_)
