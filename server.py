c8dac6579ba97408f314f6112e8e74672e59b9a5
yutong <ywan6382@uni.sydney.edu.au>
2021-03-04 21:23:17 +1100

    x

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "HA^=BeQ]Sy1--F^%vBgg@g*Q1s*1`g6<'?r""!N<KyuC~IrGFR0tF$VlKZC,_$m"
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///website2Databse.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  nm
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 import routes
